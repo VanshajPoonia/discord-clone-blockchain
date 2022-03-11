f95f9e70112713b58403fe34cc2b24f06c174e3a
Vanshaj Poonia <Vanshajtheunique@gmail.com>
2022-03-11 08:26:07 +0530

    Reverted some changes

--- client/components/Sidebar.js
+++ client/components/Sidebar.js
@@ -1,26 +1,26 @@
-import styles from '../styles/sidebar.module.css'
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-import RoomAvatar from './RoomAvatar'
+import styles from "../styles/sidebar.module.css";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import RoomAvatar from "./RoomAvatar";
 
 const Sidebar = () => {
-  const router = useRouter()
-  const [channels, setChannels] = useState([])
+  const router = useRouter();
+  const [channels, setChannels] = useState([]);
 
   useEffect(async () => {
     try {
       const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/getchannels`,
-      )
+        `${process.env.NEXT_PUBLIC_API_URL}/getchannels`
+      );
 
-      const data = await response.json()
-      setChannels(data)
+      const data = await response.json();
+      setChannels(data);
 
-      router.push(`?channel=${data[0].roomId}&name=${data[0].roomName}`)
+      router.push(`?channel=${data[0].roomId}&name=${data[0].roomName}`);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
-  }, [])
+  }, []);
 
   return (
     <div className={styles.wrapper}>
@@ -33,7 +33,7 @@ const Sidebar = () => {
         />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
